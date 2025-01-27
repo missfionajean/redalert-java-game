e3d0eb514c8dd4542c1229d3d7fc61e211bc6af0
missfionajean <missfionajean@gmail.com>
2025-01-27 10:39:48 -0500

    randomizes button colors using random binary list, refreshing each time screen is entirely green - timer functional and check for win every second, adding time if llevel is complete

---
+++ RandomSeed.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+// runnable program to test random function
+public class RandomSeed {
+    public static void main(String[] args) {
+
+        // generating seed container
+        int[] seed = new int[20];
+
+        // declaring count variables to ensure equality
+        int zeroCount = 0;
+        int oneCount = 0;
+
+        // for loop to access array index and update
+        for (int i = 0; i < seed.length; i++) {
+            // randomly generates a 1 or a 0
+            int randomBinary = (int)(Math.random() * 2);
+
+            // ensures there are exactly ten of each
+            if (randomBinary == 0) {
+                if (zeroCount < 10) {
+                    seed[i] = randomBinary;
+                    zeroCount++;
+                } else {
+                    seed[i] = 1;
+                    oneCount++;
+                }
+            } else if (randomBinary == 1) {
+                if (oneCount < 10) {
+                    seed[i] = randomBinary;
+                    oneCount++;
+                } else {
+                    seed[i] = 0;
+                    zeroCount++;
+                }
+            } 
+        }
+        
+        // prints array in string format for testing purposes
+        System.out.println(Arrays.toString(seed));
+      }
+}
---
+++ RedAlert.java
@@ -0,0 +1,155 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class RedAlert extends JFrame {
+
+    public int[] RandomSeed() {
+        // generating seed container
+        int[] seed = new int[20];
+    
+        // declaring count variables to ensure equality
+        int zeroCount = 0;
+        int oneCount = 0;
+    
+        // for loop to access array index and update
+        for (int i = 0; i < seed.length; i++) {
+            // randomly generates a 1 or a 0
+            int randomBinary = (int)(Math.random() * 2);
+    
+            // ensures there are exactly ten of each
+            if (randomBinary == 0) {
+                if (zeroCount < 10) {
+                    seed[i] = randomBinary;
+                    zeroCount++;
+                } else {
+                    seed[i] = 1;
+                    oneCount++;
+                }
+            } else if (randomBinary == 1) {
+                if (oneCount < 10) {
+                    seed[i] = randomBinary;
+                    oneCount++;
+                } else {
+                    seed[i] = 0;
+                    zeroCount++;
+                }
+            } 
+        }
+
+        // return statement so class can be called elsewhere
+        return seed;
+    }
+
+    public RedAlert() {
+        // sets header for program window
+        setTitle("Level 1 - Time: 30");
+
+        // sets "x" button functionality to close window directly
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // creates display template for elements on screen
+        setLayout(new GridLayout(5, 4));
+
+        // creates a random seed for mixing up buttons
+        int[] seed = RandomSeed();
+
+        java.util.List<JButton> allButtons = new java.util.ArrayList<>();
+
+        // creates and adds buttons to window
+        for (int i = 0; i < 20; i++) {
+            // assigns the correspodning array value to button
+            JButton button = new JButton();
+
+            // makes the button red or blue based on binary
+            if (seed[i] == 0) {
+                button.setBackground(Color.RED);
+            } else {
+                button.setBackground(Color.GREEN);
+            }
+
+            // adds a function to each button press
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (button.getBackground() == Color.RED) {
+                        button.setBackground(Color.GREEN);
+                    } else {
+                        button.setBackground(Color.RED);
+                    }
+                }
+            });
+
+            // adds the button to the visual layout
+            add(button);
+
+            // adds button to the list for management
+            allButtons.add((JButton) button);
+
+            // sets size of individual button before pack
+            button.setPreferredSize(new Dimension(120, 100));
+        }
+
+        // timer logic
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            int level = 1;
+            int countdown = 30;
+            @Override
+            public void run() {
+                if (countdown >= 0) {
+                    // check for win
+                    boolean levelComplete = true;
+
+                    // iterates through buttons checking for all green
+                    for (JButton button : allButtons) {
+                        if (button.getBackground().equals(Color.RED)) {
+                            levelComplete = false;
+                        }
+                    }
+
+                    // iterates through buttons and reandomizes them
+                    if (levelComplete) {
+                        level += 1;
+                        countdown += 4;
+                        int[] newSeed = RandomSeed();
+                        for (int i = 0; i < 20; i++) {
+                            if (newSeed[i] == 0) {
+                                allButtons.get(i).setBackground(Color.RED);
+                            } else {
+                                allButtons.get(i).setBackground(Color.GREEN);
+                            }
+                        }
+                    }
+                    // update timer
+                    setTitle("Level " + level + " - Time: " + countdown);
+                    countdown--;
+                } else {
+                    JOptionPane.showMessageDialog(null, "Final Score: " + level + "\nThanks for playing!", "GAME OVER", JOptionPane.PLAIN_MESSAGE);
+                    System.exit(0);
+                }
+            }
+        };
+
+        // schedule task to run every second (1000 miiliseconds)
+        timer.scheduleAtFixedRate(task, 0, 1000);
+
+        // sets frame to fit contents at or above preferred size
+        pack();
+
+        // shows window on screen instead of just in memory
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new RedAlert();
+            }
+        });
+    }
+}
